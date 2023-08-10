623350e3e447229a7510b0d6c8bdce19c4941049
valla <kochemarov@gmail.com>
2023-08-10 20:22:57 +0300

    permissions done

--- account/permissions.py
+++ account/permissions.py
@@ -3,9 +3,7 @@ from rest_framework.permissions import BasePermission, IsAdminUser
 
 class IsAuthorOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Разрешаем GET, HEAD или OPTIONS запросы для всех пользователей
-        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+        if request.user.is_superuser:
             return True
 
-        # Разрешаем PATCH запросы только авторизованным пользователям, которые являются авторами объекта или администраторам
-        return obj == request.user or IsAdminUser().has_permission(request, view)
+        return obj.user == request.user
