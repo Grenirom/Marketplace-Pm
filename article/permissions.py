70feac265c5a1f24de52417367d6328649e6ceec
valla <kochemarov@gmail.com>
2023-08-16 19:56:30 +0300

    articles permissoins move to seller and search changed on q

--- article/permissions.py
+++ article/permissions.py
@@ -1,22 +1,17 @@
 from rest_framework import permissions
 
 
-class IsAuthor(permissions.BasePermission):
+class IsSeller(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner
+        return request.user == request.user.is_seller
 
 
-class IsAuthorOrAdmin(permissions.BasePermission):
+class IsSellerOrAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_seller:
+            if view.action in ['create', 'update', 'partial_update', 'destroy']:
+                return True
+        return False
+
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser or request.user.is_staff:
-            return True
-        return request.user == obj.owner
-
-
-class IsAuthorOrAdminOrArticleOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser:
-            return True
-        elif request.user == obj.article.owner:
-            return True
-        return request.user == obj.owner
+        return request.user == request.user.is_seller or request.user.is_staff
--- article/views.py
+++ article/views.py
@@ -9,7 +9,7 @@ from rest_framework.filters import SearchFilter
 
 from . import serializers
 from .models import Article
-from .permissions import IsAuthorOrAdmin, IsAuthor
+from .permissions import IsSellerOrAdmin, IsSeller
 
 
 class StandartResultPagination(PageNumberPagination):
@@ -23,7 +23,16 @@ class ArticleViewSet(ModelViewSet):
     filter_backends = (DjangoFilterBackend, SearchFilter)
     search_fields = ('title', 'body')
     filterset_fields = ('owner', )
-    # permission_classes = [IsAuthorOrAdmin, ]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        search_query = self.request.query_params.get('q')
+
+        if search_query:
+            queryset = queryset.filter(title__icontains=search_query) | queryset.filter(
+                body__icontains=search_query)
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -37,12 +46,11 @@ class ArticleViewSet(ModelViewSet):
 
     def get_permissions(self):
         # удалять может только автор поста либо админы
-        if self.action == 'destroy':
-            return [IsAuthorOrAdmin(), ]
+        if self.action in ('create', 'destroy'):
+            return [IsSellerOrAdmin(), ]
         # обновлять может только автор поста
         elif self.action in ('update', 'partial_update'):
-            return [IsAuthor(), ]
+            return [IsSeller(), ]
         # просматривать могут все (list, retrive),
-        # но создавать может только залогиненный пользователь
-        return [permissions.IsAuthenticatedOrReadOnly(), ]
+        return [permissions.AllowAny(), ]
 
